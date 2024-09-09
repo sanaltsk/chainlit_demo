2ebe1c8102c6e2f7394f5bc0bef710e588e31890
Sanal <sanaltsk@gmail.com>
2024-09-08 19:15:37 -0700

    go back to gpt4o to search with image and pdfs

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
 import openai
 import os
 import chainlit as cl
+import base64
 
-# api_key = os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
 
-api_key = os.getenv("RUNPOD_API_KEY")
+# api_key = os.getenv("RUNPOD_API_KEY")
 runpod_serverless_id = os.getenv("RUNPOD_SERVERLESS_ID")
 
-# endpoint_url = "https://api.openai.com/v1"
-endpoint_url = f"https://api.runpod.ai/v2/{runpod_serverless_id}/openai/v1"
+endpoint_url = "https://api.openai.com/v1"
+# endpoint_url = f"https://api.runpod.ai/v2/{runpod_serverless_id}/openai/v1"
 
 client = openai.AsyncClient(api_key=api_key, base_url=endpoint_url)
 
 # https://platform.openai.com/docs/models/gpt-4o
-# model_kwargs = {
-#     "model": "chatgpt-4o-latest",
-#     "temperature": 1.2,
-#     "max_tokens": 500
-# }
-
 model_kwargs = {
-    "model": "mistralai/Mistral-7B-Instruct-v0.3",
-    "temperature": 0.3,
+    "model": "chatgpt-4o-latest",
+    "temperature": 1.2,
     "max_tokens": 500
 }
+
+# model_kwargs = {
+#     "model": "mistralai/Mistral-7B-Instruct-v0.3",
+#     "temperature": 0.3,
+#     "max_tokens": 500
+# }
 
 # @cl.on_message
 async def on_message_old(message: cl.Message):
@@ -52,7 +53,31 @@ async def on_message_streaming(message: cl.Message):
 async def on_message(message: cl.Message):
     #Maintain an array of messages in the user session
     message_history = cl.user_session.get("message_history", [])
-    message_history.append({'role': 'user', 'content': message.content})
+
+    #Processing images
+    images = [file for file in message.elements if "image" in file.mime] if message.elements else []
+    print(images)
+    if images:
+        #Read the image and encode it to base64
+        with open(images[0].path,"rb") as f:
+            base64_image = base64.b64encode(f.read()).decode('utf-8')
+        message_history.append({
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": message.content if message.content else "what is in the image?"
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64, {base64_image}"
+                    }
+                }
+            ]
+        })
+    else:
+        message_history.append({'role': 'user', 'content': message.content})
 
     response_msg = cl.Message(content="")
     await response_msg.send()
